9033208fe9a149ac1af39974c14f15130b690e8e
weberson22 <53545838+weberson22@users.noreply.github.com>
2023-07-25 03:26:20 -0300

    Novos teste e funções Adicionadas

--- Calculadora.Test/UnitTest1.cs
+++ Calculadora.Test/UnitTest1.cs
@@ -46,6 +46,92 @@ namespace Calculadora.Test
 
                 Assert.That(result, Is.EqualTo(3));
             }
+
+            [Test]
+            public void RaizQuadradaDeUmNumero()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.RaizQuadrada(4);
+
+                Assert.That(result, Is.EqualTo(2));
+            }
+
+            [Test]
+            public void PotenciaDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(4,2);
+
+                Assert.That(result, Is.EqualTo(16));
+            }
+
+            [Test]
+            public void PotenciaComZero()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(5, 0);
+
+                Assert.That(result, Is.EqualTo(1));
+            }
+
+            [Test]
+            public void PotenciaComNumeroNegativo()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(2, -2);
+
+                Assert.That(result, Is.EqualTo(0.25));
+            }
+
+            [Test]
+            public void PotenciaComBaseZero()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(0, 10);
+
+                Assert.That(result, Is.EqualTo(0));
+            }
+            [Test]
+            public void PotenciaComBaseUm()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(1, 100);
+
+                Assert.That(result, Is.EqualTo(1));
+            }
+            [Test]
+            public void PotenciaComExpoenteUm()
+            {
+                var sut = new CalculadoraSimples();
+
+                var result = sut.Potencia(5, 1);
+
+                Assert.That(result, Is.EqualTo(5));
+            }
+
+            [Test]
+            public void RaizQuadradaDeNumeroNegativo()
+            {
+                var sut = new CalculadoraSimples();
+
+                Assert.Throws<ArgumentException>(() => sut.RaizQuadrada(-4));
+            }
+
+            [Test]
+            public void TestDividirPorZero()
+            {
+                // Arrange
+                var sut = new CalculadoraSimples();
+
+                // Act & Assert
+                Assert.Throws<ArgumentException>(() => sut.Dividir(8, 0)); // Não é possível dividir por zero
+            }
         }
     }
 }
--- Calculadora/CalculadoraSimples.cs
+++ Calculadora/CalculadoraSimples.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Calculadora
 {
-    public class CalculadoraSimples
+    public interface ICalculadoraBasica
+    {
+        int Somar(int num1, int num2);
+        int Subtrair(int num1, int num2);
+        int Multiplicar(int num1, int num2);
+        double Dividir(int num1, int num2);
+    }
+
+    public interface ICalculadoraAvancada
+    {
+        double RaizQuadrada(double number);
+        double Potencia(double baseNumber, int exponent);
+    }
+    public class CalculadoraSimples : ICalculadoraBasica, ICalculadoraAvancada
     {
         public int Somar(int num1, int num2)
         {
@@ -23,9 +36,27 @@ namespace Calculadora
             return num1 * num2;
         }
 
-        public int Dividir(int num1, int num2)
+        public double Dividir(int num1, int num2)
         {
+            if (num2 == 0)
+            {
+                throw new ArgumentException("Não é possível dividir por zero.");
+            }
             return num1 / num2;
+        }
+
+        public double RaizQuadrada(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Não é possível calcular a raiz quadrada de um número negativo.");
+            }
+            return Math.Sqrt(number);
+        }
+
+        public double Potencia(double baseNumber, int exponent)
+        {
+            return Math.Pow(baseNumber, exponent);
         }
 
     }
